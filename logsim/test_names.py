4d4f8688d261c53fac5b583cebc54268268e5a2b
mhr36 <105633242+mhr36@users.noreply.github.com>
2022-05-23 10:53:30 +0100

    Added lookup and query tests

--- logsim/test_names.py
+++ logsim/test_names.py
@@ -1,1 +1,22 @@
+"""Test the names module"""
+import pytest
 
+from names import Names
+
+@pytest.fixture
+def names_with_entries():
+    new_names = Names()
+    IDs = new_names.lookup(["And1", "Nor1", "Sw1"])
+    return new_names
+
+
+def test_lookup(names_with_entries):
+    assert [0,1,2] == new_names.lookup(["And1", "Nor1", "Sw1"])
+    
+    assert [3] == new_names.lookup(["string"])
+    
+
+def test_query(names_with_entries):
+    assert [0,1,2] == [new_names.query(item) for item in ["And1", "Nor1", "Sw1"]]
+    
+    assert (new_names.query(["string"]) is None)

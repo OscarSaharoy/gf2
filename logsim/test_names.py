02c7baf74765ee752082e6b2c9ad5f8809d921fe
mhr36 <105633242+mhr36@users.noreply.github.com>
2022-05-23 13:26:01 +0100

    pep8

--- logsim/test_names.py
+++ logsim/test_names.py
@@ -14,9 +14,10 @@ def names_with_entries():
 def test_lookup(names_with_entries):
     new_names = Names()
     assert [0] == new_names.lookup(["Zero"])  # Adding new name
-    assert [0, 1] == new_names.lookup(["Zero", "One"])  # An existing name and a new name
-    assert [2, 3] == new_names.lookup(["Two", "Three"]) # Two new names
-    assert [2, 1] == new_names.lookup(["Two", "One"])  #Two existing names
+    assert [0, 1] == new_names.lookup(["Zero", "One"])  
+    # An existing name and a new name
+    assert [2, 3] == new_names.lookup(["Two", "Three"])  # Two new names
+    assert [2, 1] == new_names.lookup(["Two", "One"])  # Two existing names
 
 
 def test_query(names_with_entries):

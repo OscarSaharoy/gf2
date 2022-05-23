e973a8f24b345fae53921582ac789e829b62eece
Oscar <osaharoy@gmail.com>
2022-05-23 10:31:49 +0100

    implement names.lookup

--- logsim/names.py
+++ logsim/names.py
@@ -41,6 +41,8 @@ class Names:
     def __init__(self):
         """Initialise names list."""
         self.error_code_count = 0  # how many error codes have been declared
+        self.name_count = 0
+        self.name_map = {}
 
     def unique_error_codes(self, num_error_codes):
         """Return a list of unique integer error codes."""
@@ -56,11 +58,24 @@ class Names:
         If the name string is not present in the names list, return None.
         """
 
-    def lookup(self, name_string_list):
+    def lookup(self, name_list):
         """Return a list of name IDs for each name string in name_string_list.
 
         If the name string is not present in the names list, add it.
         """
+
+        if not all(isinstance(item, str) for item in name_list):
+            raise ValueError("All items in argument of Names.lookup must be strings")
+
+        new_names = list(set(name_list) - set(self.name_map.keys()))
+
+        new_names_map = {new_name: i + self.name_count
+                         for i, new_name in enumerate(new_names)}
+        self.name_count += len(new_names)
+
+        self.name_map = {**self.name_map, **new_names_map}
+
+        return [self.name_map[name] for name in name_list]
 
     def get_name_string(self, name_id):
         """Return the corresponding name string for name_id.

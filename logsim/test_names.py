f619023867ce550b59d9582d31a6f1a4e1589a94
mhr36 <105633242+mhr36@users.noreply.github.com>
2022-05-23 13:43:40 +0100

    Commenting, Clarity

--- logsim/test_names.py
+++ logsim/test_names.py
@@ -4,25 +4,24 @@ import pytest
 from names import Names
 
 
-@pytest.fixture
-def names_with_entries():
-    new_names = Names()
-    new_names.lookup(["Zero", "One", "Two"])
-    return new_names
-
-
 def test_lookup():
     new_names = Names()
     assert [0] == new_names.lookup(["Zero"])  # Adding new name
-    assert [0, 1] == new_names.lookup(["Zero", "One"])
     # An existing name and a new name
+    assert [0, 1] == new_names.lookup(["Zero", "One"])
     [ID2, ID3] = new_names.lookup(["Two", "Three"])  # Two new names
+    # Check that the IDs for the names are correct
     assert [ID2, ID3] == new_names.lookup(["Two", "Three"])
     assert [ID3, 0, ID2, 1] == new_names.lookup(
         ["Three", "Zero", "Two", "One"])  # All four names random order
 
 
-def test_query(names_with_entries):
-    assert [0, 1, 2] == [names_with_entries.query(item)
+def test_query():
+    new_names = Names()
+    IDs = new_names.lookup(["Zero", "One", "Two"])  # Populate names
+    # Test query for a set of present names
+    assert IDs == [names_with_entries.query(item)
                          for item in ["Zero", "One", "Two"]]
+    # Check that querying an absent name returns None
     assert (names_with_entries.query("nonexistant") is None)
+    

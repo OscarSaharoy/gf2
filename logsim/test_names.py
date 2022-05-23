d2f25df61e4eee76a6144c7000141b71f8ec2e0a
mhr36 <105633242+mhr36@users.noreply.github.com>
2022-05-23 10:58:00 +0100

    Update test_names.py

--- logsim/test_names.py
+++ logsim/test_names.py
@@ -3,6 +3,7 @@ import pytest
 
 from names import Names
 
+
 @pytest.fixture
 def names_with_entries():
     new_names = Names()
@@ -11,12 +12,10 @@ def names_with_entries():
 
 
 def test_lookup(names_with_entries):
-    assert [0,1,2] == names_with_entries.lookup(["And1", "Nor1", "Sw1"])
-    
+    assert [0, 1, 2] == names_with_entries.lookup(["And1", "Nor1", "Sw1"])
     assert [3] == names_with_entries.lookup(["string"])
-    
+
 
 def test_query(names_with_entries):
-    assert [0,1,2] == [names_with_entries.query(item) for item in ["And1", "Nor1", "Sw1"]]
-    
+    assert [0, 1, 2] == [names_with_entries.query(item) for item in ["And1", "Nor1", "Sw1"]]
     assert (names_with_entries.query(["string"]) is None)

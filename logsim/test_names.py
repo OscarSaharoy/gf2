8755b305deda1c2d91ea3386f9172e8616a13da3
mhr36 <105633242+mhr36@users.noreply.github.com>
2022-05-23 13:37:49 +0100

    Accept multiple names added in different orders

--- logsim/test_names.py
+++ logsim/test_names.py
@@ -11,16 +11,18 @@ def names_with_entries():
     return new_names
 
 
-def test_lookup(names_with_entries):
+def test_lookup():
     new_names = Names()
     assert [0] == new_names.lookup(["Zero"])  # Adding new name
     assert [0, 1] == new_names.lookup(["Zero", "One"])
     # An existing name and a new name
-    assert [2, 3] == new_names.lookup(["Two", "Three"])  # Two new names
-    assert [2, 1] == new_names.lookup(["Two", "One"])  # Two existing names
+    IDs = new_names.lookup(["Two", "Three"])  # Two new names
+    assert IDs == new_names.lookup(["Two", "Three"])
+    assert [3, 0, 2, 1] == new_names.lookup(
+        ["Three", "Zero", "Two", "One"])  # All four names random order
 
 
 def test_query(names_with_entries):
     assert [0, 1, 2] == [names_with_entries.query(item)
                          for item in ["Zero", "One", "Two"]]
-    assert (names_with_entries.query("string") is None)
+    assert (names_with_entries.query("nonexistant") is None)

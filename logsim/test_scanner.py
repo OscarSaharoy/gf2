54a64137cd57c75460709e4cbcbb90b84d21fb6d
Oscar <osaharoy@gmail.com>
2022-05-28 21:38:09 +0100

    pep8

--- logsim/test_scanner.py
+++ logsim/test_scanner.py
@@ -1,7 +1,8 @@
 """Test the scanner module"""
-import pytest
+# import pytest
 from scanner import Scanner, Symbol
 from names import Names
+
 
 def test_example_symbols():
 
@@ -9,9 +10,9 @@ def test_example_symbols():
     new_scanner = Scanner("logsim/tests/example1.txt", new_names)
 
     EOF = Symbol(sym_type=new_scanner.EOF)
-    
+
     symbols = [new_scanner.get_symbol()]
-    
+
     while not symbols[-1] == EOF:
         new_symbol = new_scanner.get_symbol()
         symbols.append(new_symbol)

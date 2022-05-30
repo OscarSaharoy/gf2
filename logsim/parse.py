ce4351df4121e481a6126f5d1e057e92c39991ad
mhr36 <105633242+mhr36@users.noreply.github.com>
2022-05-30 17:19:56 +0100

    pep8

--- logsim/parse.py
+++ logsim/parse.py
@@ -199,7 +199,7 @@ class Parser:
         self.parse_literal(ARROW)
         rhs_signal_name, rh2_signal_pin = self.parse_signal()
         self.parse_literal(SEMICOLON)
-        
+
         lhs_pin_id = rhs_pin_id = None
         if lhs_signal_pin is not None:
             lhs_pin_id = lhs_signal_pin.id
